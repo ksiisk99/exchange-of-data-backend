f538220e6b9b41cc18cfebce5fcc781bcf9c1f20
Sangin <ksiisk99@gmail.com>
2022-09-14 01:31:56 +0900

    시간 데이터 자동 매핑 기능

---
+++ src/main/java/com/ay/exchange/common/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.ay.exchange.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdDate;
+    @LastModifiedBy
+    private String lastModifiedDate;
+}
---
+++ src/main/java/com/ay/exchange/user/controller/UserController.java
@@ -0,0 +1,2 @@
+package com.ay.exchange.user.controller;public class UserController {
+}
