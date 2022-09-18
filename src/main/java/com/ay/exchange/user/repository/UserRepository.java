c12d17d6d1e24ee56362452298ee7f32418f21c9
Sangin <ksiisk99@gmail.com>
2022-09-19 02:05:00 +0900

    로그인 로직 수정

--- src/main/java/com/ay/exchange/user/repository/UserRepository.java
+++ src/main/java/com/ay/exchange/user/repository/UserRepository.java
@@ -6,13 +6,15 @@ import com.ay.exchange.user.dto.response.SignUpResponse;
 import com.ay.exchange.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,String> {
-    Optional<UserInfoDto> findByUserIdAndPassword(String userId, String password);
 
     boolean existsByEmail(String email);
 
     boolean existsByNickName(String nickName);
+
+    Optional<UserInfoDto> findUserInfoByUserId(String userId);
 
 }
